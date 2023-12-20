e07f4a8736f06286b5f25f34738dd115ab7f1781
abubakar2000 <abubakar.mughal2ooo@icloud.com>
2023-12-20 14:10:22 +0500

    studio

--- apps/billing/web/src/app/pages/app/suppliers/Suppliers.tsx
+++ apps/billing/web/src/app/pages/app/suppliers/Suppliers.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import Modal from '../../../shared/organisms/Modal';
-import { ChangeEvent, useCallback, useState } from 'react';
-import { Button } from '@fluentui/react-components';
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
+import { Button, Divider } from '@fluentui/react-components';
 import { getLastRouteItem, handleChange } from '../../../utils/common';
 import InputField from '../../../shared/molecules/InputField';
 import { ISupplier } from '@billinglib';
@@ -25,10 +25,16 @@ const Suppliers = () => {
     TRNNumber: '',
     whatsapps: '',
   });
+  const [showAdditionalDetails, setShowAdditionalDetails] = useState(false);
 
   const toggleModel = useCallback(
     () => setIsCreatingRecord(!isCreatingRecord),
     [isCreatingRecord]
+  );
+
+  const toggleAdditionalDetails = useCallback(
+    () => setShowAdditionalDetails(!showAdditionalDetails),
+    [showAdditionalDetails]
   );
 
   const handleOnChange = useCallback(
@@ -39,6 +45,14 @@ const Suppliers = () => {
         newSupplier,
         setNewSupplier
       );
+    },
+    [newSupplier]
+  );
+
+  const handleSubmit = useCallback(
+    (ev: FormEvent<HTMLFormElement>) => {
+      ev.preventDefault();
+      console.log(JSON.stringify(newSupplier, null, 2));
     },
     [newSupplier]
   );
@@ -54,18 +68,158 @@ const Suppliers = () => {
         title="Add Supplier"
         triggerButton={<Button onClick={toggleModel}>Add New</Button>}
       >
-        <form>
-          <InputField
-            name="emails"
-            value={newSupplier?.emails}
-            onChange={handleOnChange}
-            label=""
-            placeholder=""
-            type="text"
-            fieldSize="large"
-            labelSize="medium"
-            required
-          />
+        <form className="gap-2 flex flex-col" onSubmit={handleSubmit}>
+          {!showAdditionalDetails && (
+            <>
+              <InputField
+                name="emails"
+                value={newSupplier?.emails}
+                onChange={handleOnChange}
+                label="Email"
+                placeholder="Supplier email"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+                required
+              />
+              <InputField
+                name="name"
+                value={newSupplier?.name}
+                onChange={handleOnChange}
+                label="Supplier Name"
+                placeholder="Enter name"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+                required
+              />
+              <InputField
+                name="city"
+                value={newSupplier?.city}
+                onChange={handleOnChange}
+                label="Supplier City"
+                placeholder="Enter city"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+                required
+              />
+              <InputField
+                name="telephones"
+                value={newSupplier?.telephones}
+                onChange={handleOnChange}
+                label="Supplier Telephone"
+                placeholder="Enter telephone"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+                required
+              />
+              <InputField
+                name="addressLine1"
+                value={newSupplier?.addressLine1}
+                onChange={handleOnChange}
+                label="Address Line 1"
+                placeholder="Enter address line 1"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+                required
+              />
+              <InputField
+                name="addressLine2"
+                value={newSupplier?.addressLine2}
+                onChange={handleOnChange}
+                label="Address Line 2"
+                placeholder="Enter address line 2 (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <Divider className="my-3" />
+            </>
+          )}
+          <Button onClick={toggleAdditionalDetails}>
+            {showAdditionalDetails ? 'Hide Optionals' : 'Show Optionals'}
+          </Button>
+          {showAdditionalDetails && (
+            <>
+              <InputField
+                name="whatsapps"
+                value={newSupplier?.whatsapps}
+                onChange={handleOnChange}
+                label="Whatsapp number "
+                placeholder="Enter whatsapp number (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="NTN"
+                value={newSupplier?.NTN}
+                onChange={handleOnChange}
+                label="Supplier NTN"
+                placeholder="Enter supplier NTN (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="STN"
+                value={newSupplier?.STN}
+                onChange={handleOnChange}
+                label="Supplier STN"
+                placeholder="Enter supplier STN (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="licenseNumber"
+                value={newSupplier?.licenseNumber}
+                onChange={handleOnChange}
+                label="Supplier License number"
+                placeholder="Enter license number (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="licenseNumber"
+                value={newSupplier?.licenseNumber}
+                onChange={handleOnChange}
+                label="Supplier License number"
+                placeholder="Enter license number (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="TNNumber"
+                value={newSupplier?.TNNumber}
+                onChange={handleOnChange}
+                label="Supplier TN Number"
+                placeholder="Enter TN number (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+              <InputField
+                name="TRNNumber"
+                value={newSupplier?.TNNumber}
+                onChange={handleOnChange}
+                label="Supplier TRN Number"
+                placeholder="Enter TRN number (Optional)"
+                type="text"
+                fieldSize="large"
+                labelSize="medium"
+              />
+            </>
+          )}
+          <Divider className="my-3" />
+          <Button type="submit" size="large" appearance="primary">
+            Submit
+          </Button>
         </form>
       </Modal>
     </div>
