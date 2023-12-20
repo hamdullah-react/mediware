1a9809264374b5271ddb05737b4ef66a42062b34
abubakar2000 <abubakar.mughal2ooo@icloud.com>
2023-12-20 18:28:00 +0500

    frontend batch entry

--- apps/billing/lib/src/lib/billing-libs.ts
+++ apps/billing/lib/src/lib/billing-libs.ts
@@ -3,7 +3,7 @@ export function billingLibs(): string {
 }
 
 export interface ISupplier {
-  id?: string;
+  id?: number;
   emails: string;
   name: string;
   city: string;
@@ -19,4 +19,50 @@ export interface ISupplier {
   createdAt?: Date;
   updatedAt?: Date;
   deletedAt?: Date;
+}
+
+export interface IMedicine {
+  id?: number;
+  name: string;
+  formula?: string;
+  brand?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  InvoiceMedicine: IInvoiceMedicine[];
+}
+
+export interface IInvoice {
+  id: number;
+  invoiceNumber: string;
+  invoiceData: Date;
+  total: number;
+  InvoiceMedicine: IInvoiceMedicine;
+  salesTax: number;
+  supplierId: number;
+  Supplier: ISupplier;
+  deliveredBy: string;
+  bookingDriver: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+}
+
+export interface IInvoiceMedicine {
+  id: number;
+  batchIdentifier: string;
+  medicineId: number;
+  Medicine: IMedicine;
+  invoiceId: number;
+  Invoice: IInvoice;
+  quantity: number;
+  pack: string;
+  expiry: Date;
+  unitTakePrice: number;
+  unitSalePrice: number;
+  discountPercentage: number;
+  discountedAmount: number;
+  advTaxPercentatge: number;
+  netAmount: number;
 }
