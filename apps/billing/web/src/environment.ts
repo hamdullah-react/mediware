d766462870b46f4c22f35e6f7e8e32617b18cb2d
abubakar2000 <abubakar.mughal2ooo@icloud.com>
2023-12-27 19:34:18 +0500

    no env

--- apps/billing/web/src/environment.ts
+++ apps/billing/web/src/environment.ts
@@ -1,5 +1,5 @@
-export const HOST = 'http://192.168.1.127:3000';
-// export const HOST = 'http://127.0.0.1:3000';
+// export const HOST = 'http://192.168.1.127:3000';
+export const HOST = 'http://127.0.0.1:3000';
 export const INFO = {
   name: 'Mediware',
   description: 'Your Pharma Partner',
